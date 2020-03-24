b5395ce7926731ab17df7326ebc3f5b7811839d6
tillahoffmann <tillahoffmann@gmail.com>
2020-03-24 12:05:17 +0000

    Deploying to gh-pages from  - bfa1a149306878205ba27f649c204bd4fdcb974f 🚀

---
+++ precache-manifest.fa293f8a3db17a66ca59f763687f3c37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3299b8d5f67e8454e42b9b6d0e78b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3adbe57d1ea5ad7eac29",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3e24b201422b3c764d7",
+    "url": "/static/js/2.5d4df72b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d4df72b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3adbe57d1ea5ad7eac29",
+    "url": "/static/js/main.543fb885.chunk.js"
+  },
+  {
+    "revision": "c3601dcbab2fb8fe3887",
+    "url": "/static/js/runtime-main.45b7d06f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
